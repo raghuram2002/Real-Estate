02af4892d09fb3be81c2b90f6a1a1eaeefbf2ef5
Raghu <raghukotthapalli14@gmail.com>
2025-02-15 10:33:23 +0530

    initial commit

---
+++ src/components/Header.jsx
@@ -0,0 +1,30 @@
+import banner from '../assets/banner2.jpg'
+import Navbar from "./Navbar.jsx";
+
+const Header = () => {
+    return (
+        <div className="min-vh-100 w-100" style={{
+            backgroundImage: `url(${banner})`,
+            backgroundSize: 'cover',
+            backgroundRepeat: 'no-repeat',
+            backgroundPosition: 'center'
+        }}>
+
+            <div
+                className="position-absolute top-0 start-0 w-100 h-100"
+                style={{backgroundColor: "rgba(0, 0, 0, 0.5)"}} // Adjust opacity (0 = fully transparent, 1 = fully visible)
+            ></div>
+
+            <Navbar/>
+            <div className='d-flex flex-column justify-content-center container position-relative text-center text-white ' style={{height:'595px'}}>
+                <h2 className='fw-bolder fs-1 mb-4'>Explore homes that fit your dreams</h2>
+                <div className='d-flex justify-content-center gap-2'>
+                    <button className='btn border text-white' style={{width:'110px'}}>Projects</button>
+                    <button className='btn btn-primary' style={{width: '110px'}}>Contact Us</button>
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default Header;
---
+++ src/components/Navbar.jsx
@@ -0,0 +1,40 @@
+import logo from '../assets/logo.png'
+import '../index.css'
+const Navbar = () => {
+  return (
+      <>
+        <nav className="navbar navbar-expand-lg navbar-dark">
+            <div className="container d-flex align-items-center justify-content-between">
+                <a className='navbar-brand d-flex align-items-center' href="#">
+                    <img className='rounded-circle me-2' src={logo} alt="" width='40px'/>RaghuEstates
+                </a>
+                <button className='navbar-toggler' type='button' data-bs-toggle='collapse' data-bs-target='#navbarText'
+                        aria-controls='navbarText' aria-expanded='false' aria-label='Toggle navigation'>
+                    <span className='navbar-toggler-icon'></span>
+                </button>
+                <div className="collapse navbar-collapse rounded-5 text-center justify-content-between align-items-center" id="navbarText">
+                    <ul className="navbar-nav mx-auto d-flex justify-content-center gap-3">
+                        <li className="nav-item">
+                            <a className='nav-link' href="#Header">Home</a>
+                        </li>
+                        <li className="nav-item">
+                            <a className='nav-link' href="#">About</a>
+                        </li>
+                        <li className="nav-item">
+                            <a className='nav-link' href="#">Projects</a>
+                        </li>
+                        <li className="nav-item">
+                            <a className='nav-link' href="#">Testimonials</a>
+                        </li>
+                    </ul>
+                    <div className='d-lg-block d-flex justify-content-center align-items-center mt-2 mt-lg-0'>
+                        <button className='btn btn-light rounded-5'>Sign up</button>
+                    </div>
+                </div>
+            </div>
+        </nav>
+      </>
+  )
+}
+
+export default Navbar
